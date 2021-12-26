c9bd495d269dda9d48dfa73466bb4666f22f2b2c
Anastasiia <lytvynana@gmail.com>
2021-12-26 16:12:19 +0200

    done

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,76 @@
 <script>
-	export let name;
+  import { writable } from "svelte/store";
+  let isLoading = false;
+  let result = writable("");
+  const formData = {};
+  const submit = async () => {
+    isLoading = true;
+    const response = await fetch("/api/sendMail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formData),
+    });
+    const responseJSON = await response.json();
+    result.set(responseJSON.result.success ? "email was sent" : "error occured");
+    isLoading = false;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  {#if isLoading}
+    <div class="loader">
+      <img src="../loader.gif" alt="loader" />
+    </div>
+  {:else}
+    <form on:submit|preventDefault={submit}>
+      <input
+        name="email"
+        type="email"
+        placeholder="Enter your email"
+        bind:value={formData.email}
+      />
+      <input
+        name="name"
+        type="text"
+        placeholder="Enter your name"
+        bind:value={formData.name}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Enter your password"
+        bind:value={formData.password}
+      />
+      <input
+        name="confirmPassword"
+        type="password"
+        placeholder="Confirm password"
+        bind:value={formData.confirmPassword}
+      />
+      <input type="submit" disabled={isLoading} value="Send"/>
+    </form>
+    <div>
+      <p>{$result}</p>
+    </div>
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  form{
+    border: 1px solid #333;
+    padding: 20px;
+    border-radius: 5px;
+    box-shadow: 10px 5px 5px rgb(83, 83, 83,0.4);
+  }
+  input{
+      border: 1px solid #666;
+  }
 </style>
