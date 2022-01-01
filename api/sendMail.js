a029329d4bf8d70ad2c0f8b03c56004cc2667d7f
Anastasiia Lytvyn <lytvynana@gmail.com>
2022-01-01 16:00:56 +0200

    more rate limit

--- api/sendMail.js
+++ api/sendMail.js
@@ -69,7 +69,7 @@ const validate = (body) => {
 
 module.exports = async (req, res) => {
   try {
-    rateLimit(req.headers["x-real-ip"], 1);
+    rateLimit(req.headers["x-real-ip"], 3);
     validate(req.body);
     const result = await formSubmit(req.body);
     return res.json({ result });
